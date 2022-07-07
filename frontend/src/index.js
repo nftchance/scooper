8a3a1abd45f3f7677ba79095081476f5d07d2f2e
nftchance <chance@utc24.io>
2022-07-07 15:36:24 -0500

    repairing stripped dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
